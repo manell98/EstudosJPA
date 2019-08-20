a70f5d743bb96094020b81e3093f2fbcd865c2db
Emanuell Santos <dfmanu06@gmail.com>
2019-08-20 19:58:56 -0300

    Revisao JPQL

--- src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -33,33 +33,30 @@ public class ProdutoDao {
 	}
 
 	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
-		Root<Produto> root = query.from(Produto.class);
-
-		Path<String> nomePath = root.<String> get("nome");
-		Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
-		Path<Integer> categoriaPath = root.join("categorias").<Integer> get("id");
-
-		List<Predicate> predicates = new ArrayList<>();
-
-		if (!nome.isEmpty()) {
-			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
-			predicates.add(nomeIgual);
-		}
-		if (categoriaId != null) {
-			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
-			predicates.add(categoriaIgual);
-		}
-		if (lojaId != null) {
-			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-			predicates.add(lojaIgual);
-		}
-
-		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
-
-		TypedQuery<Produto> typedQuery = em.createQuery(query);
-		return typedQuery.getResultList();
+		
+		String jpql = "select p from Produto p ";
+		
+		if(categoriaId != null)
+			jpql += "join fetch p.categorias c where c.id = :pCategoriaId and ";
+		else
+			jpql += " where ";
+		if(lojaId != null)
+			jpql += "p.loja.id = :pLojaId and ";
+		if(!nome.isEmpty())
+			jpql += "p.nome like :pProdutoNome and ";
+		
+		jpql += " 1 = 1";
+		
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		
+		if(categoriaId != null)
+			query.setParameter("pCategoriaId", categoriaId);
+		if(lojaId != null)
+			query.setParameter("pLojaId", lojaId);
+		if(!nome.isEmpty())
+			query.setParameter("pProdutoNome", nome);
+		
+		return query.getResultList();
 
 	}
 
