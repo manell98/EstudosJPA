bf3f16416ddead3671db645e6f00ac9f0c16e16d
Emanuell Santos <dfmanu06@gmail.com>
2019-08-22 16:59:30 -0300

    Lock otimista

--- src/main/java/br/com/caelum/model/Produto.java
+++ src/main/java/br/com/caelum/model/Produto.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Version;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
@@ -27,6 +28,9 @@ public class Produto {
 	@NotEmpty
 	private String linkDaFoto;
 	
+	@Version
+	private int versao;
+
 	@NotEmpty
 	@Column(columnDefinition="TEXT")
 	private String descricao;
@@ -95,6 +99,14 @@ public class Produto {
 
 	public Loja getLoja() {
 		return loja;
+	}
+	
+	public int getVersao() {
+		return versao;
+	}
+
+	public void setVersao(int versao) {
+		this.versao = versao;
 	}
 
 }
